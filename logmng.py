15960eceebb23f5be2b5f59941ff60039fb539d7
Ankit Rusia <sowhatim@hotmail.com>
2016-07-13 23:48:01 +0530

    Update logmng.py

--- logmng.py
+++ logmng.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python3
 import os,subprocess,time
 
-pth='/home/crux/git' #Enter the path for log management here
-mx = 1024*1024*25 #for 25 M i.e. Max size of the logs
+pths=['/whatever/path/','/pth2'] #Enter the paths for directories to set log management for
+mx = 25 #for 25 MB i.e. Max size of the logs
 nm = 7 # Number of Log archives to maintain
 
 
 def fetchLogs():
     logs = []
-    for root, dirs, files in os.walk(pth):
-        for file in files:
-            if file.endswith(".log"):
-                logs.append(os.path.join(root, file))
     logsc = []
+    for pth in pths:
+        for root, dirs, files in os.walk(pth):
+            for file in files:
+                if file.endswith(".log"):
+                    logs.append(os.path.join(root, file))
     for l in logs:
-        if os.path.getsize(l) > mx:
+        if os.path.getsize(l) > (mx/(1024*1024)):
             logsc.append(l)
     return logsc
 
@@ -29,12 +30,11 @@ def archiveLogs():
             if os.path.isfile(os.path.join(fPath,i)) and fil in i and ".gz" in i:
                 filist.append(os.path.join(fPath,i))
                 cnt += 1
-        print (cnt, sorted(filist,key=os.path.getmtime)[0])
         if cnt > nm:
             os.remove(sorted(filist,key=os.path.getmtime)[0])
         os.chdir(fPath)
         subprocess.call(["gzip --suffix " + '.'.join([str(i) for i in time.localtime(time.time())[1:5]]) + ".gz --best " + fil + "; echo > " + fil],shell=True)
 
 
-logsc = fetchLogs()
+logsc = list(set(fetchLogs()))
 archiveLogs()
